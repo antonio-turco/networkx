29ddd2b94114d9ad9f93f83ec6ba4264210a9186
Antonio <truock@gmail.com>
2020-04-09 20:21:40 +0200

    :recycle: :bug: improves readability by using more small focused functions. It better handles the heuristic input function

--- networkx/algorithms/shortest_paths/greedy.py
+++ networkx/algorithms/shortest_paths/greedy.py
@@ -5,7 +5,11 @@ import networkx as nx
 __all__ = ["greedy_path"]
 
 def greedy_path(G, source, target, heuristic = None, weight = 'weight'):
-    #add initial checks, like if the source is correct and if the target is present
+    if heuristic != None:
+        h = lambda node: heuristic(node, target)
+    else:
+        h = lambda node: 1        
+
     instant = count()
     queue = [(0, next(instant), source, None) ]
     visited = {(source, 0): None}
@@ -13,28 +17,32 @@ def greedy_path(G, source, target, heuristic = None, weight = 'weight'):
         _, _, element, parent = heappop(queue)
 
         if element == target:
-            #new function, get_path
-            path = [element]
-            while parent != None:
-                path.append(parent)
-                parent = visited[parent]
-            path.reverse()
-            return path
+            return get_path(element, parent, visited)
         
-        if element in visited:
-            continue
-        else: 
+        if not element in visited:
             visited[element] = parent
-
-        neighbourhood = []
-        if element in G:
-            neighbourhood = G[element]
-
-        for neighbour in neighbourhood:
-            cost_to_neighbour = heuristic(neighbour)
-            neighbour_record = ( cost_to_neighbour, next(instant), neighbour, element)
-            heappush(queue, neighbour_record)
+            neighbourhood = get_neighbourhood(G, element)
+            push_neighbourhood(queue, neighbourhood, element, h, instant)
 
         
     raise nx.NetworkXNoPath(f"Node {target} is not connected with {source} ")
 
+def get_path( element, parent, visited ):
+    path = [element]
+    while parent != None:
+        path.append(parent)
+        parent = visited[parent]
+    path.reverse()
+    return path
+
+def get_neighbourhood(g, element):
+    neighbourhood = []
+    if element in g:
+        neighbourhood = g[element]
+    return neighbourhood
+
+def push_neighbourhood(queue, neighbourhood, parent, h, instant):
+    for neighbour in neighbourhood:
+        cost_to_neighbour = h(neighbour)
+        neighbour_record = ( cost_to_neighbour, next(instant), neighbour, parent)
+        heappush(queue, neighbour_record)
