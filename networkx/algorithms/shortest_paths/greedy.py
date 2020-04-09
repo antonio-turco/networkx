f913fdcaedd11073589631667add10b919746c84
Antonio <truock@gmail.com>
2020-04-09 14:32:01 +0200

    :bug: :beers: fixes some errors and improves the implementation of the greedy search. Note that the heurstic has not been yet implemented

--- networkx/algorithms/shortest_paths/greedy.py
+++ networkx/algorithms/shortest_paths/greedy.py
@@ -5,31 +5,31 @@ import networkx as nx
 __all__ = ["greedy_path"]
 
 def greedy_path(G, source, target, heuristic = None, weight = 'weight'):
-    print("greedy hello!")
-
-    '''
-    
-    create queue with source
-    while not empty queue
-        take first
-        if is target --> stop
-        else 
-            take the neighbourhood
-            take the dist and sum it to the actual cost
-            place them in the queue
-
-    '''
-
+    #add initial checks, like if the source is correct and if the target is present
     instant = count()
     queue = [(0, next(instant), source, None) ]
-    visited = {}
+    visited = {(source, 0): None}
     while queue:
         cost, _, element, parent = heappop(queue)
+
         if element == target:
-            #must return path, so you need to scan all the parents chain
-            return element, parent
+            #new function, get_path
+            path = [element]
+            parent, cost_from_source = visited[element, cost]
+            while cost_from_source != 0:
+                path.append(parent)
+                parent, cost_from_source = visited[parent, cost_from_source]
+            path.append(parent)
+            path.reverse()
+            return path
+
         neighbourhood = G[element]
+
         for neighbour in neighbourhood:
-            if not neighbour in visited:
-                neighbour_record = (cost + G[element][neighbour][weight], next(instant), neighbour, element)
-                heappush(queue, neighbour_record)
+            cost_to_neighbour = cost + G[element][neighbour][weight]
+            visited[neighbour, cost_to_neighbour] = element, cost
+            neighbour_record = ( cost_to_neighbour, next(instant), neighbour, element)
+            heappush(queue, neighbour_record)
+    
+    raise nx.NetworkXNoPath(f"Node {target} is not connected with {source} ")
+
